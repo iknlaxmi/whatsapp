b4ce325777bcdd562ff847ff254035248b9c9463
iknlaxmi <iknlaxmi@yahoo.com>
2023-07-27 12:58:16 +0530

    corrected

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import ChatRoom from './components/ChatRoom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TEST</h1>
+      <ChatRoom />
+    </div>
+  );
+}
+
+export default App;
